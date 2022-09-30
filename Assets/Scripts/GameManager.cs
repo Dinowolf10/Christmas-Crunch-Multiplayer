2f3a1c7e009c95a590de9a171f6cec6c726452c3
Zaven Kazandjian <Dinowolf10@gmail.com>
2022-09-30 12:16:59 -0400

    Imported Photon Fusion and started setting up coop menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fusion;
+using Fusion.Sockets;
+using System;
 
 /// <summary>
 /// file: GameManager.cs
 /// description: Manages the progress of the overarching game and scene loading
 /// author: Nathan Ballay
 /// </summary>
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour, INetworkRunnerCallbacks
 {
     // Reference set in inspector
     [SerializeField]
@@ -33,6 +36,48 @@ public class GameManager : MonoBehaviour
     [SerializeField] float betweenWaitTime;
     private float betweenTimer;
     private int numGameManagers;
+
+    // Networking callbacks
+    private NetworkRunner _runner;
+    [SerializeField] private NetworkPrefabRef _playerPrefab;
+    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        if (runner.IsServer)
+        {
+            // Create a unique position for the player
+            Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
+            NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+            // Keep track of the player avatars so we can remove it when they disconnect
+            _spawnedCharacters.Add(player, networkPlayerObject);
+        }
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        // Find and remove the players avatar
+        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            _spawnedCharacters.Remove(player);
+        }
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
 
     private void Awake()
     {
@@ -118,7 +163,7 @@ public class GameManager : MonoBehaviour
         else
         {
             // Find the index of a game that hasn't been played yet
-            while (playedScenes.Contains(idx = Random.Range(3, 10)))
+            while (playedScenes.Contains(idx = UnityEngine.Random.Range(3, 10)))
             {
                 continue;
             }
@@ -272,5 +317,31 @@ public class GameManager : MonoBehaviour
     public int GetRoundNumber()
     {
         return roundNumber;
+    }
+
+    async void StartGame(GameMode mode)
+    {
+        // Create the Fusion runner and let it know that we will be providing user input
+        _runner = gameObject.AddComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+
+        // Start or join (depends on gamemode) a session with a specific name
+        await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "TestRoom",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+
+    public void HostCoopGame()
+    {
+        StartGame(GameMode.Host);
+    }
+
+    public void JoinCoopGame()
+    {
+        StartGame(GameMode.Client);
     }
 }
